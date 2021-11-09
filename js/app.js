ddb6ee85a40e782a4654750bba25746e518002bc
Ignacio Navarrete <jnavarrete@novasolutionsystems.com>
2021-11-09 16:49:36 -0600

    agregando agstos

--- js/app.js
+++ js/app.js
@@ -20,6 +20,12 @@ class Presupuesto {
     this.restante = Number(presupuesto);
     this.gastos = [];
   }
+
+  nuevoGasto(gasto) {
+    this.gastos = [...this.gastos, gasto];
+    console.log(this.gastos);
+    ui.inprimirAlerta("correcto");
+  }
 }
 
 //Imprimir html
@@ -51,9 +57,47 @@ class UI {
 
     document.querySelector(".primario").insertBefore(divMensaje, formulario);
 
-    setTimeout(()=>{
-        divMensaje.remove();
-    }, 3000)
+    setTimeout(() => {
+      divMensaje.remove();
+    }, 3000);
+  }
+
+  agregarGastoListado(gastos) {
+    this.limpiarHTML();
+    //Iterar sobre los gastos
+
+    gastos.forEach((gasto) => {
+      const { cantidad, nombre, id } = gasto;
+      //Crear un li
+      const nuevoGasto = document.createElement("li");
+      nuevoGasto.classList.add(
+        "list-group-item",
+        "d-flex",
+        "justify-content-between",
+        "align-items-center"
+      );
+      nuevoGasto.dataset.id = id;
+
+      //Agregar el html del gasto
+      nuevoGasto.innerHTML = `${nombre}<span class="badge badge-primary badge-pill">${cantidad}</span>`;
+      // Boton para borrar
+
+      const btnBorrar = document.createElement("button");
+      console.log(btnBorrar);
+      btnBorrar.classList.add("btn", "btn-danger", "borrar-gasto");
+      btnBorrar.textContent = "borrar x";
+
+      nuevoGasto.appendChild(btnBorrar);
+      //Agregar al html
+
+      gatoListado.appendChild(nuevoGasto);
+    });
+  }
+
+  limpiarHTML() {
+    while (gatoListado.firstChild) {
+      gatoListado.removeChild(gatoListado.firstChild);
+    }
   }
 }
 
@@ -82,13 +126,24 @@ function agregarGasto(e) {
   e.preventDefault();
 
   const nombre = document.querySelector("#gasto").value;
-  const cantidad = document.querySelector("#cantidad").value;
+  const cantidad = Number(document.querySelector("#cantidad").value);
 
   if (nombre === "" && cantidad === "") {
     ui.inprimirAlerta("Ambos casos son obligatorios", "error");
     return;
-} else if (cantidad <= 0 || isNaN(cantidad)){
+  } else if (cantidad <= 0 || isNaN(cantidad)) {
     ui.inprimirAlerta("cantidad no valida", "error");
     return;
   }
+
+  //Genera un objeto con el gastom object literal
+  const gasto = { nombre, cantidad, id: Date.now() };
+  presupuesto.nuevoGasto(gasto);
+
+  //Imprimir los gastos
+  const { gastos } = presupuesto;
+  ui.agregarGastoListado(gastos);
+
+  //Reiniciar el formulario
+  formulario.reset();
 }
